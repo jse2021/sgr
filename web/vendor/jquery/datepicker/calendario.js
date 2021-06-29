3a71cb67aea1af6f33b060965d3510e8de7a6ca3
jose <maldonadojose201422@gmail.com>
2021-06-29 00:33:59 -0300

    sigo con datepicker

--- web/vendor/jquery/datepicker/calendario.js
+++ web/vendor/jquery/datepicker/calendario.js
@@ -1,10 +1,33 @@
-$(function() {
-    $(".datepicker").datepicker({
-        dateFormat: "yy-mm-dd",
-        dayNamesMin: ["Dom", "Lun", "Mar", "Mier", "Jue", "Vie", "Sab"],
-        monthNamesShort: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
-        changeMonth: true,
-        changeYear: true,
+$(document).ready(function() {
+    var green = ["2021-06-30"];
+
+    $(function() {
+        $(".datepicker").datepicker({
+            dateFormat: "yy-mm-dd",
+            dayNamesMin: ["Dom", "Lun", "Mar", "Mier", "Jue", "Vie", "Sab"],
+            monthNamesShort: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+            changeMonth: true,
+            changeYear: true,
+            maxDate: '+2Y',
+            minDate: '-4d',
+
+            beforeShowDay: function(date) {
+                var dateISO = $.datepicker.formatDate('yy-mm-dd', date);
+                //console.log(dateISO);
+
+                // if (red.indexOf(dateISO) > -1) {
+                //     return [true, "red"] // Enabled, class
+                // }
+                // if (yellow.indexOf(dateISO) > -1) {
+                //     return [true, "yellow"]
+                // }
+                if (green.indexOf(dateISO) > -1) {
+                    return [true, "green"]
+                }
+                return [true] // If not found, must at least return the enabled boolean.
+            }
+        });
+
     });
 
 });

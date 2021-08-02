2246476b91f7c0c75387a1966fc728b7bd624469
jose <maldonadojose201422@gmail.com>
2021-08-01 22:37:00 -0300

    tratando de capturar la cancha

--- src/AppBundle/Controller/gestionReservasController.php
+++ src/AppBundle/Controller/gestionReservasController.php
@@ -11,7 +11,7 @@ use Doctrine\ORM\EntityRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\Mapping as ORM;
 use AppBundle\Repository\reservaRepository;
-
+use Symfony\Component\Validator\Constraints\Length;
 
 /**
  *@Route ("/gestionReservas")
@@ -19,56 +19,58 @@ use AppBundle\Repository\reservaRepository;
 
 class gestionReservasController extends Controller 
 {
-  function pruebaNativeQuery()
+  /**
+   * @Route("/buscoCancha", name="buscoCancha")
+   */
+  public function pruebaNativeQuery(Request $request)
   {
-    
-    $data = array("grande"=>"tamano");
-    $json = json_encode($data);
-    $x=$data['grande'];
+      $x = "chica";   
+      $array = array();
+      $canchaJson = $request->request->get('test', 'no hay valor');    
+      $array = json_decode($canchaJson,true);
+      dump($canchaJson);
       $reservas = $this->getDoctrine()->getRepository('AppBundle:Reserva')->obtenerNativeQueryDeReserva($x);
       // Devuelto en formato JSON
       $response = new Response();
       $response->setContent(json_encode($reservas));
       $response->headers->set('Content-Type', 'application/json');
       dump($response);
+      
       return $response;
   }
-
   /**
    * @Route("/nuevaReserva", name="nuevaReserva")
    */
-   public function nuevaReservaAction(Request $request) {     
-   $this->pruebaNativeQuery();
-  //  dump($this->pruebaNativeQuery());
+  public function nuevaReservaAction(Request $request) {     
    $nReserva = new reserva();
    $form = $this->createForm(nuevaReservaType::class,$nReserva);       
-  
-
-    $form ->handleRequest($request);    
-
-      if ($form->isSubmitted() && $form->isValid()) {
-        $reserva = $form->getData();
-        $nombre = $form->getData(['nombre']);
-        dump($nombre);
-        $em = $this->getDoctrine()->getManager();
-        $em ->persist($reserva,$nombre);
-        $em->flush();
-        $this->addFlash('success','Reserva creada con éxito');          
-        return $this->redirectToRoute('nuevaReserva');
-      } 
+   $this->pruebaNativeQuery($request);
+   
+   $form ->handleRequest($request);    
+   if ($form->isSubmitted() && $form->isValid()) {
+      $reserva = $form->getData();
+      $nombre = $form->getData(['nombre']);
+      dump($nombre);
+      $em = $this->getDoctrine()->getManager();
+      $em ->persist($reserva,$nombre);
+      $em->flush();
+      $this->addFlash('success','Reserva creada con éxito');          
+    
+    return $this->redirectToRoute('nuevaReserva');
+    } 
+   
     return $this->render('gestionReservas/nuevaReserva.html.twig',array('form' => $form->createView()));
   }
-
 
   /**
    * @Route("/consultarReservaPorCliente", name="consultarReservaPorCliente")
    */
-   public function consultarReservaPorClienteAction(Request $request)
-  {
-      $nReserva = new reserva();
-      /*construyo formulario*/
-      $form = $this->createForm(consultarReservaPorClienteType::class,$nReserva);
-      return $this->render('gestionReservas/consultarReservaPorCliente.html.twig',array('form' => $form->createView()));
+  public function consultarReservaPorClienteAction(Request $request){
+    $nReserva = new reserva();
+    /*construyo formulario*/
+    $form = $this->createForm(consultarReservaPorClienteType::class,$nReserva);
+    
+    return $this->render('gestionReservas/consultarReservaPorCliente.html.twig',array('form' => $form->createView()));
   }
 
   /**
@@ -91,65 +93,6 @@ class gestionReservasController extends Controller
       // replace this example code with whatever you need
       return $this->render('gestionReservas/consultarReservaPorCancha.html.twig',array('form' => $form->createView()));
   }
-
-  
-
 }
 
 ?>
-<script>
-      $(document).ready(function (){
-    
-        var green = ["2021-07-29"];
-        $(function() {
-        pulsacion();
-        $(".datepicker").datepicker({
-        
-          dateFormat: "yy-mm-dd",
-          dayNamesMin: ["Dom", "Lun", "Mar", "Mier", "Jue", "Vie", "Sab"],
-          monthNamesShort: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
-          changeMonth: true,
-          changeYear: true,
-          maxDate: '+2Y',
-          minDate: '-4d',
-
-            beforeShowDay: function(date) {
-                var dateISO = $.datepicker.formatDate('yy-mm-dd', date);
-                //console.log(dateISO);
-
-                // if (red.indexOf(dateISO) > -1) {
-                //     return [true, "red"] // Enabled, class
-                // }
-                // if (yellow.indexOf(dateISO) > -1) {
-                //     return [true, "yellow"]
-                // }
-                if (green.indexOf(dateISO) > -1) {
-                    return [true, "green"]
-                }
-                return [true] // If not found, must at least return the enabled boolean.
-            }
-    });
-  });
-  $('.timepicker').timepicker({
-        timeFormat: 'HH:mm',
-        interval: 60,
-        minTime: '9:00',
-        maxTime: '23:00',
-        dropdown: true,
-        scrollbar: true,
-
-
-    });
-
-});
-
-      function pulsacion() {
-        $("#nueva_reserva_cancha").click(function (){
-          var Vcancha = {cancha:$("#nueva_reserva_cancha").val()}
-          if (Vcancha.cancha == "chica") {
-            
-          }
-        });
-
-      }   
-    </script>
--- web/vendor/jquery/datepicker/calendario.js
+++ web/vendor/jquery/datepicker/calendario.js
@@ -1,51 +1,72 @@
-    $(document).ready(function() {
-
-        var green = ["2021-07-29"];
-        $(function() {
-            pulsacion();
-            $(".datepicker").datepicker({
-
-                dateFormat: "yy-mm-dd",
-                dayNamesMin: ["Dom", "Lun", "Mar", "Mier", "Jue", "Vie", "Sab"],
-                monthNamesShort: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
-                changeMonth: true,
-                changeYear: true,
-                maxDate: '+2Y',
-                minDate: '-4d',
-
-                beforeShowDay: function(date) {
-                    var dateISO = $.datepicker.formatDate('yy-mm-dd', date);
-                    //console.log(dateISO);
-
-                    // if (red.indexOf(dateISO) > -1) {
-                    //     return [true, "red"] // Enabled, class
-                    // }
-                    // if (yellow.indexOf(dateISO) > -1) {
-                    //     return [true, "yellow"]
-                    // }
-                    if (green.indexOf(dateISO) > -1) {
-                        return [true, "green"]
-                    }
-                    return [true] // If not found, must at least return the enabled boolean.
-                }
-            });
+$(document).ready(function() {
+    $("#nueva_reserva_cancha").click(function() {
+        var Vcancha = {
+            cancha: $("#nueva_reserva_cancha").val()
+        }
+        var pasoUrl = '/buscoCancha';
+        $.ajax({
+            url: pasoUrl,
+            type: 'post',
+            dataType: 'json',
+            data: {
+                'test': Vcancha.cancha
+            },
+            success: function(data) {}
         });
-        $('.timepicker').timepicker({
-            timeFormat: 'HH:mm',
-            interval: 60,
-            minTime: '9:00',
-            maxTime: '23:00',
-            dropdown: true,
-            scrollbar: true,
-        });
+
+        $("#nueva_reserva").append($('<p>' + Vcancha.cancha + '</p>'));
+
     });
 
-    function pulsacion() {
-        $("#nueva_reserva_cancha").click(function() {
-            var Vcancha = { cancha: $("#nueva_reserva_cancha").val() }
-            if (Vcancha.cancha == "chica") {
+    levantoCalendario();
+    levantoHorario();
 
+});
+// Fin DocumentReady
+
+$(function levantoHorario() {
+    $('.timepicker').timepicker({
+        timeFormat: 'HH:mm',
+        interval: 60,
+        minTime: '9:00',
+        maxTime: '23:00',
+        dropdown: true,
+        scrollbar: true,
+    });
+});
+// Fin hora
+
+$(function levantoCalendario() {
+    var green = ["2021-07-29"];
+    $(".datepicker").datepicker({
+        dateFormat: "yy-mm-dd",
+        dayNamesMin: ["Dom", "Lun", "Mar", "Mier", "Jue", "Vie", "Sab"],
+        monthNamesShort: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+        changeMonth: true,
+        changeYear: true,
+        maxDate: '+2Y',
+        minDate: '-4d',
+
+        beforeShowDay: function(date) {
+            var dateISO = $.datepicker.formatDate('yy-mm-dd', date);
+            //console.log(dateISO);
+
+            // if (red.indexOf(dateISO) > -1) {
+            //     return [true, "red"] // Enabled, class
+            // }
+            // if (yellow.indexOf(dateISO) > -1) {
+            //     return [true, "yellow"]
+            // }
+            if (green.indexOf(dateISO) > -1) {
+                return [true, "green"]
             }
-        });
+            return [true] // If not found, must at least return the enabled boolean.
+        }
+    });
+});
+// Fin Calendario
+// function pulsacion() {
 
-    }
+//     // Fin ajax
+
+// }
